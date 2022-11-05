4074bea3c341514b529ce0557154ad29b59c6f65
minneelyyyy <aprilkahny@gmail.com>
2022-11-04 23:46:48 -0400

    implement getargs() for x86_64

--- include/common.h
+++ include/common.h
@@ -9,9 +9,12 @@
 			asm("leal 0x8(%%ebp), %0" : "=r" (argv));	\
 		} while (0)
 #elif defined (__x86_64__)
-#error "sorry, x86_64 not implemented yet!"
 // warning: only use inside of _start
-#	define getargs(argc, argv) ((void) 0)
+#	define getargs(argc, argv)\
+		do {\
+			asm("movl 0x8(%%rbp), %0" : "=r" (argc));\
+			asm("leaq 0x12(%%rbp), %0" : "=r" %argv));\
+		} while (0)
 #else
 #error "only x86 and x86_64 are supported"
 #endif
