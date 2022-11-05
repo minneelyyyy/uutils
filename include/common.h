380c62e7acbc87f2d6fc98c97180d681ce0dd09b
minneelyyyy <aprilkahny@gmail.com>
2022-11-04 23:54:02 -0500

    systemcall prototypes match FreeBSD

--- include/common.h
+++ include/common.h
@@ -10,20 +10,22 @@
 		} while (0)
 
 typedef unsigned int size_t;
+typedef signed int ssize_t;
 #elif defined(__x86_64__)
 // warning: only use inside of _start, run before any other code
-#	define getargs(argc, argv)\
-		do {\
-			asm("movl (%%rdi), %0" : "=r" (argc));\
-			asm("leaq 8(%%rdi), %0" : "=r" (argv));\
+#	define getargs(argc, argv)				\
+		do {						\
+			asm("movl (%%rdi), %0" : "=r" (argc));	\
+			asm("leaq 8(%%rdi), %0" : "=r" (argv));	\
 		} while (0)
 
 typedef unsigned long size_t;
+typedef signed long ssize_t;
 #else
 #error "only x86 and x86_64 are supported"
 #endif
 
-extern unsigned int sys_write(int fd, void* buf, unsigned int len);
-extern void sys_exit(int exit_status);
+extern ssize_t sys_write(int fd, void* buf, size_t nbyte);
+extern void sys_exit(int rval);
 
 #endif /* __COMMON_H */
