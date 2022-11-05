33cffb0f75a701f838787cf36b26005b4e2720c7
minneelyyyy <aprilkahny@gmail.com>
2022-11-04 23:32:42 -0500

    added size_t to common.h

--- include/common.h
+++ include/common.h
@@ -8,13 +8,17 @@
 			asm("movl 0x4(%%ebp), %0" : "=r" (argc));	\
 			asm("leal 0x8(%%ebp), %0" : "=r" (argv));	\
 		} while (0)
-#elif defined (__x86_64__)
-// warning: only use inside of _start
+
+typedef unsigned int size_t;
+#elif defined(__x86_64__)
+// warning: only use inside of _start, run before any other code
 #	define getargs(argc, argv)\
 		do {\
 			asm("movl (%%rdi), %0" : "=r" (argc));\
 			asm("leaq 8(%%rdi), %0" : "=r" (argv));\
 		} while (0)
+
+typedef unsigned long size_t;
 #else
 #error "only x86 and x86_64 are supported"
 #endif
