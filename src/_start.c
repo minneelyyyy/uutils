748ffc867628ca14cde17aa7a8952f15c62c3ece
minneelyyyy <aprilkahny@gmail.com>
2022-12-06 12:17:53 -0600

    move getargs macro to _start.c

--- src/_start.c
+++ src/_start.c
@@ -1,6 +1,23 @@
 #include <common.h>
 #include <bufio.h>
 
+#if defined(__i386__)
+/* warning: only use inside of _start */
+#	define getargs(argc, argv)					\
+		do {							\
+			asm("movl 0x4(%%ebp), %0" : "=r" (argc));	\
+			asm("leal 0x8(%%ebp), %0" : "=r" (argv));	\
+		} while (0)
+#elif defined(__x86_64__)
+/* warning: only use inside of _start, run before any other code */
+#	define getargs(argc, argv)				\
+		do {						\
+			asm("movl (%%rdi), %0" : "=r" (argc));	\
+			asm("leaq 8(%%rdi), %0" : "=r" (argv));	\
+		} while (0)
+#error "only x86 and x86_64 are supported"
+#endif
+
 extern int main(int argc, char** argv);
 
 void _start(void) {
