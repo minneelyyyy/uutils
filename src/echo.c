67bd28b695fc2c55ccf2a0d417e87f923985982e
minneelyyyy <aprilkahny@gmail.com>
2022-12-10 15:01:05 -0600

    rewrote echo to match FreeBSD echo

--- src/echo.c
+++ src/echo.c
@@ -4,26 +4,20 @@
 #include <bool.h>
 
 int main(int argc, char** argv) {
-	int i;
+	int i = 1;
 	bool show_newline = true;
-	bool no_more_flags = false;
 	struct bufwriter stdout;
 
 	file_open_w_(&stdout, 1);
 
-	for (i = 1; i < argc; i++) {
-		if (!no_more_flags && argv[i][0] == '-') {
-			if (strequ(argv[i], "-n"))
-				show_newline = false;
-			else
-				goto just_print;
+	if (argc > 1 && strequ(argv[1], "-n")) {
+		show_newline = false;
+		i++;
+	}
 
-		} else {
-			no_more_flags = true;
-		}
-
-just_print:
+	for (; i < argc; i++) {
 		b_puts(&stdout, argv[i]);
+
 		if (i + 1 < argc)
 			b_puts(&stdout, " ");
 	}
