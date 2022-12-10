a79ac0b4ee7d3731833936469e51e56961eeb669
minneelyyyy <aprilkahny@gmail.com>
2022-12-10 15:58:07 -0500

    Update defs.h

--- include/defs.h
+++ include/defs.h
@@ -7,8 +7,11 @@ typedef unsigned int size_t;
 typedef signed int ssize_t;
 
 #define WORDSIZE 2
+#define WORD short
 #define DWORDSIZE 4
+#define DWORD int
 #define MAXWORDSIZE DWORDSIZE
+#define MAXWORD DWORD
 
 #define LITTLE_ENDIAN
 
@@ -17,9 +20,13 @@ typedef unsigned long size_t;
 typedef signed long ssize_t;
 
 #define WORDSIZE 2
+#define WORD short
 #define DWORDSIZE 4
+#define DWORD int
 #define QWORDSIZE 8
+#define QWORD long
 #define MAXWORDSIZE QWORDSIZE
+#define MAXWORD QWORD
 
 #define LITTLE_ENDIAN
 
