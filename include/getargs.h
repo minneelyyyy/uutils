f1ba5962dfab2bcc6a53684638154230984e4a6d
minneelyyyy <aprilkahny@gmail.com>
2022-12-10 15:16:51 -0600

    move getargs to its own header file

---
+++ include/getargs.h
@@ -0,0 +1,21 @@
+#ifndef __GETARGS_H
+#define __GETARGS_H
+
+/* getargs() macros only work inside of start,
+ * and should be the first bit of code run */
+#if defined(__i386__)
+#	define getargs(argc, argv)					\
+		do {							\
+			asm("movl 0x4(%%ebp), %0" : "=r" (argc));	\
+			asm("leal 0x8(%%ebp), %0" : "=r" (argv));	\
+		} while (0)
+#elif defined(__x86_64__)
+#	define getargs(argc, argv)				\
+		do {						\
+			asm("movl (%%rdi), %0" : "=r" (argc));	\
+			asm("leaq 8(%%rdi), %0" : "=r" (argv));	\
+		} while (0)
+#error "only x86 and x86_64 are supported"
+#endif
+
+#endif /* __GETARGS_H */
--- src/_start.c
+++ src/_start.c
@@ -1,22 +1,5 @@
-#include <common.h>
-#include <bufio.h>
-
-/* getargs() macros only work inside of start,
- * and should be the first bit of code run */
-#if defined(__i386__)
-#	define getargs(argc, argv)					\
-		do {							\
-			asm("movl 0x4(%%ebp), %0" : "=r" (argc));	\
-			asm("leal 0x8(%%ebp), %0" : "=r" (argv));	\
-		} while (0)
-#elif defined(__x86_64__)
-#	define getargs(argc, argv)				\
-		do {						\
-			asm("movl (%%rdi), %0" : "=r" (argc));	\
-			asm("leaq 8(%%rdi), %0" : "=r" (argv));	\
-		} while (0)
-#error "only x86 and x86_64 are supported"
-#endif
+#include <syscalls.h>
+#include <getargs.h>
 
 extern int main(int argc, char** argv);
 
