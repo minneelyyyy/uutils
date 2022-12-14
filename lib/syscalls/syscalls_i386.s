f13421e5a82fa99f1d12a90a2faa121b8526681c
minneelyyyy <aprilkahny@gmail.com>
2022-12-14 15:33:35 -0600

    set errno in syscall wrappers

--- lib/syscalls/syscalls_i386.s
+++ lib/syscalls/syscalls_i386.s
@@ -1,3 +1,4 @@
+.extern errno
 
 .global sys_exit
 sys_exit:
@@ -8,29 +9,47 @@ sys_exit:
 sys_read:
 	mov $3, %eax
 	int $0x80
+	jc .read_error
+	ret
+
+	.read_error:
+	mov %eax, errno
+	mov $0xFFFFFFFF, %eax
 	ret
 
 .global sys_write
 sys_write:
 	mov $4, %eax
 	int $0x80
+	jc .write_error
+	ret
+
+	.write_error:
+	mov %eax, errno
+	mov $0xFFFFFFFF, %eax
 	ret
 
 .global sys_open
 sys_open:
 	mov $5, %eax
 	int $0x80
+	jc .open_error
+	ret
+
+	.open_error:
+	mov %eax, errno
+	mov $0xFFFFFFFF, %eax
 	ret
 
 .global sys_close
 sys_close:
 	mov $6, %eax
 	int $0x80
+	jc .close_error
 	ret
 
-.global sys_fcntl
-sys_fcntl:
-	mov $92, %eax
-	int $0x80
+	.close_error:
+	mov %eax, errno
+	mov $0xFFFFFFFF, %eax
 	ret
 
